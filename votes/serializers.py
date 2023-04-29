1a69b6437cccea07c58af4d98e367cedbffb2dec
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-04-29 20:28:54 +0300

    Refactoring likes & dislikes of Vote model

--- votes/serializers.py
+++ votes/serializers.py
@@ -4,9 +4,36 @@ from votes.models import Vote
 
 
 class VoteSerializer(serializers.ModelSerializer):
-    up_vote_by = serializers.ReadOnlyField(source="up_vote_by.username")
-    down_vote_by = serializers.ReadOnlyField(source="down_vote_by.username")
 
     class Meta:
         model = Vote
         fields = ("id", "post", "up_vote_by", "down_vote_by")
+
+
+class VoteListSerializer(VoteSerializer):
+    up_vote_by = serializers.ReadOnlyField(
+        source="up_vote_by.username"
+    )
+    down_vote_by = serializers.ReadOnlyField(
+        source="down_vote_by.username"
+    )
+
+
+class VoteDetailSerializer(serializers.ModelSerializer):
+    up_vote_by_username = serializers.ReadOnlyField(
+        source="up_vote_by.username"
+    )
+    down_vote_by_username = serializers.ReadOnlyField(
+        source="down_vote_by.username"
+    )
+
+    class Meta:
+        model = Vote
+        fields = (
+            "id",
+            "post",
+            "up_vote_by",
+            "up_vote_by_username",
+            "down_vote_by",
+            "down_vote_by_username"
+        )
--- votes/views.py
+++ votes/views.py
@@ -6,7 +6,11 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from posts.models import Post
 from votes.models import Vote
 from votes.permissions import HasSelfVotedOrReadOnly
-from votes.serializers import VoteSerializer
+from votes.serializers import (
+    VoteSerializer,
+    VoteListSerializer,
+    VoteDetailSerializer
+)
 
 
 class VoteViewSet(viewsets.ModelViewSet):
@@ -15,10 +19,25 @@ class VoteViewSet(viewsets.ModelViewSet):
     )
     serializer_class = VoteSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticatedOrReadOnly, HasSelfVotedOrReadOnly, )
+    permission_classes = (IsAuthenticatedOrReadOnly, HasSelfVotedOrReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return VoteListSerializer
+
+        if self.action == "retrieve":
+            return VoteDetailSerializer
+
+        return self.serializer_class
 
     def perform_create(self, serializer):
         post_instance = get_object_or_404(Post, pk=self.request.data["post"])
+
+        if post_instance.author == self.request.user:
+            raise serializers.ValidationError(
+                {"message": "You can’t evaluate your own posts"}
+            )
+
         if self.request.data["up_vote_by"]:
             already_up_voted = Vote.objects.filter(
                 post=post_instance, up_vote_by=self.request.user
