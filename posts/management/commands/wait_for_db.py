63a0ba1350ef44897352e907839d68fa696dd5dd
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-05-07 15:16:06 +0300

    Moved the management folder with contains to the posts app

--- posts/management/commands/wait_for_db.py
+++ posts/management/commands/wait_for_db.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
 
         while not db_connect:
             try:
-                db_connect = connections["default"]
+                db_connect = connections["default"].cursor()
             except OperationalError:
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
